def1501994581d25e279d53591ed94345fd56792
paiva73 <mau.paiva10@gmail.com>
2023-08-27 15:39:56 -0300

    ejercicio finalizado

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const stringArray = strangeArray.filter((element) => {
+    return typeof element === 'string';
+  })
+
+  showList(stringArray.sort((a, b) => a.localeCompare(b)));
+});
